030bc1322c612eb7009beb8083b4cc2906b4b046
ElankumaranR <elankumaran2103@gmail.com>
2024-05-22 23:08:33 +0530

    c2

--- movie/src/main/java/movie/controller.java
+++ movie/src/main/java/movie/controller.java
@@ -1,23 +1,16 @@
 package movie;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
 public class controller {
 
     @GetMapping("/")
-    public ResponseEntity<Resource> getIndex() throws IOException {
-        Resource resource = new ClassPathResource("static/build/index.html");
-        if (resource.exists()) {
-            return ResponseEntity.ok().body(resource);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    public void home(){
+        
     }
 }
