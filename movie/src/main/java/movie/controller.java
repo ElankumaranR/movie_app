f81a22b01de129b1daf440dd0f89810ffa71cf7b
ELANKUMARANR <elankumaran2103@gmail.com>
2024-07-29 21:21:12 +0530

    -d

--- movie/src/main/java/movie/controller.java
+++ movie/src/main/java/movie/controller.java
@@ -1,27 +1,24 @@
 package movie;
 import movie.app.User;
 
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000") 
+@CrossOrigin("http://localhost:3000")
 public class controller {
 
     @PostMapping("/login")
-    public String login(@RequestBody User user){
-        if("user".equals(user.getUsername())&&"password".equals(user.getPassword())){
-            return "login Successfull";
-        }
-        else{
-            return "invalid credentials";
+    public String login(@RequestBody User user) {
+        if ("user".equals(user.getUsername()) && "password".equals(user.getPassword())) {
+            return "Login Successful";
+        } else {
+            return "Invalid credentials";
         }
     }
 }
