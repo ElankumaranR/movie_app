bfb4981ba6e665d28d7a78df44575c794f090ae5
ELANKUMARANR <elankumaran2103@gmail.com>
2024-07-30 00:03:27 +0530

    -m

--- movie/src/test/java/movie/example/MovieApplicationTests.java
+++ movie/src/test/java/movie/example/MovieApplicationTests.java
@@ -1,13 +1,17 @@
 package movie.example;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Rollback(false)
+@Transactional  // Ensures that the transactions are rolled back after each test
 class MovieApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
